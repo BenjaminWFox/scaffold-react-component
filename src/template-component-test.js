101aea0874fb29a2ff36abaa36fe4ebc6ce38908
Ben Fox <BenjaminWFox@gmail.com>
2020-09-25 08:01:47 -0700

    Remove empty trailing spaces from test template

--- src/template-component-test.js
+++ src/template-component-test.js
@@ -2,7 +2,7 @@ import __StubComponentName__ from '.'
 
 describe('The __StubComponentName__ component', () => {
   // TODO: Implement a real test
-  it('Should import successfully', () => {    
+  it('Should import successfully', () => {
     console.warn('TODO: Implement a real test', __StubComponentName__)
 
     expect(__StubComponentName__).toBeDefined()
